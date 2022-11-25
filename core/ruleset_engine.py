64f2fcf5b8e3c30cc53fb92246316b125392bc61
O72 <aljaloudo@gmail.com>
2022-11-25 16:34:36 -0500

    performance enhancement

--- core/ruleset_engine.py
+++ core/ruleset_engine.py
@@ -4,10 +4,11 @@ import yaml
 
 # This class is responsible of applying each of the rulesets to each of the js source code lines.
 class Rule:
-    def __init__(self, filename, line, line_number):
+    def __init__(self, filename, line, line_number, rules):
         self.filename = filename
         self.line = line.strip()
         self.line_number = line_number
+        self.rules = rules
         self.ruleset_engine()
 
     def ruleset_engine(self):
@@ -15,13 +16,11 @@ class Rule:
         This function alternate over each of the rulesets on ruleset.yaml file and scan it against each js line.
         :return:
         """
-        with open('core/ruleset.yaml', 'r') as ruleset:
-            rules = yaml.safe_load(ruleset)
-            for rule in rules:
-                location = f'{self.filename}#{self.line_number}'
-                issue = re.search(rf"{rules[rule]['regex']}", self.line,
-                                  re.IGNORECASE)
-                if issue:
-                    Format(self.filename, self.line, self.line_number, rules[rule]['issue_type'], rules[rule]['severity']
-                           , rules[rule]['confidence'], rules[rule]['CWE'], location, rules[rule]['description'],
-                           rules[rule]['remediation'])
+        for rule in self.rules:
+            location = f'{self.filename}#{self.line_number}'
+            issue = re.search(rf"{self.rules[rule]['regex']}", self.line,
+                              re.IGNORECASE)
+            if issue:
+                Format(self.filename, self.line, self.line_number, self.rules[rule]['issue_type'], self.rules[rule]['severity']
+                       , self.rules[rule]['confidence'], self.rules[rule]['CWE'], location, self.rules[rule]['description'],
+                       self.rules[rule]['remediation'])
