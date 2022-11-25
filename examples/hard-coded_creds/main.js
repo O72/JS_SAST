dd8af03261686eed709152fa3d7822636599f614
O72 <aljaloudo@gmail.com>
2022-11-25 16:35:11 -0500

    typos

--- examples/hard-coded_creds/main.js
+++ examples/hard-coded_creds/main.js
@@ -1,6 +1,6 @@
 function main() {
     let password = "Super secret password";
-    let email = "omar@example.com"
+    let email = "omar@example.com";
     console.log(password);
     console.log(email);
     let token = "asfsfs3r356232hm1337"
